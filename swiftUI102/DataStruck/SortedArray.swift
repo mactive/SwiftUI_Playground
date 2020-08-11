416eca3797286e9c0d0e1440e1587a326bdd09f0
mactive <mactive@gmail.com>
2020-08-11 10:09:17 +0800

    add randomAccessCollection protocol

--- swiftUI102/DataStruck/SortedArray.swift
+++ swiftUI102/DataStruck/SortedArray.swift
@@ -7,15 +7,33 @@
 //
 
 import Foundation
-struct SortedArray<Element>: CustomStringConvertible where Element:  Comparable {
+struct SortedArray<Element>: CustomStringConvertible, RandomAccessCollection where Element:  Comparable {
     private var items = [Element]()
     var count: Int { items.count}
     var description: String { items.description }
+    var startIndex: Int { items.startIndex }
+    var endIndex: Int { items.endIndex }
+    
+    subscript(position: Int) -> Element {
+        items[position]
+    }
     
     mutating func insert(_ element: Element) {
+        for (i, item) in items.enumerated() {
+            if item >= element {
+                print("element \(element)")
+                items.insert(element, at: i)
+                return
+            }
+        }
         items.append(element)
-        items.sort()
     }
+    
+    // 在没有增加 RandomAccessCollection 之前的insert实现
+//    mutating func insert(_ element: Element) {
+//        items.append(element)
+//        items.sort()
+//    }
     
     mutating func remove(at index: Int) {
         items.remove(at: index)
